a2585de1e834f7eb46fce757d59b2cb815435e4c
Axel Venet <kern046@gmail.com>
2017-08-02 03:57:57 +0200

    Initial Next server and pages

---
+++ src/server.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const express = require('express');
+const next = require('next');
+//const apiHandler = require('./api')
+
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({
+  dev,
+  dir: path.resolve(__dirname + '/client'),
+})
+const nextHandler = app.getRequestHandler()
+
+app.prepare().then(() => {
+  const server = express()
+
+//  server.use('/api', apiHandler);
+  server.get('*', (req, res) => {
+    return nextHandler(req, res);
+  });
+
+  server.listen(80, err => {
+    if (err) throw err
+    console.log('> Ready on http://127.0.0.1')
+  });
+});
