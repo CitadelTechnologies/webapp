e91752de97041e6cb3f3db534ed3984899abbf76
Axel Venet <kern046@gmail.com>
2018-01-02 20:36:20 +0100

    Implement seeds for projects

---
+++ seeds/3_topics.js
@@ -0,0 +1,44 @@
+exports.seed = (knex, Promise) => knex('topic__topics').del().then(() =>
+  knex('topic__topics').insert([
+    {
+      id: 1,
+      name: 'Politique',
+      slug: 'politique',
+      description: '',
+      picto: '',
+      picture: ''
+    },
+    {
+      id: 2,
+      name: 'Économie',
+      slug: 'economie',
+      description: '',
+      picto: '',
+      picture: ''
+    },
+    {
+      id: 3,
+      name: 'Environnement',
+      slug: 'environnement',
+      description: '',
+      picto: '',
+      picture: ''
+    },
+    {
+      id: 4,
+      name: 'Culture',
+      slug: 'culture',
+      description: '',
+      picto: '',
+      picture: ''
+    },
+    {
+      id: 5,
+      name: 'Société',
+      slug: 'societe',
+      description: '',
+      picto: '',
+      picture: ''
+    },
+  ]);
+);
