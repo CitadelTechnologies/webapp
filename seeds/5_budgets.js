b572f576b48109b1e0cf69982e5b27333a97658f
Axel Venet <kern046@gmail.com>
2018-04-10 23:58:36 +0200

    Implement budget seeds for main projects

--- seeds/5_budgets.js
+++ seeds/5_budgets.js
@@ -1,9 +1,20 @@
 const { budget } = require('../src/api/managers');
 
-exports.seed = (knex, Promise) => budget.deleteBudget("budget-de-la-citadelle")
-    .then(response => budget.createBudget("Budget de la Citadelle", "Budget global du mouvement"))
-    .then(response => Promise.all([
-        budget.createSector("budget-de-la-citadelle", "Projets"),
-        budget.createSector("budget-de-la-citadelle", "Infrastructure"),
-    ]))
-;
+exports.seed = (knex, Promise) => Promise.all([
+    budget.deleteBudget("budget-de-la-citadelle")
+        .then(response => budget.createBudget("Budget de la Citadelle", "Budget global du mouvement"))
+        .then(response => Promise.all([
+            budget.createSector("budget-de-la-citadelle", "Projets"),
+            budget.createSector("budget-de-la-citadelle", "Infrastructure"),
+        ])),
+    budget.deleteBudget("budget-projet-craftcamp")
+        .then(response => budget.createBudget("Budget Projet craftcamp", "Budget du projet CraftCamp"))
+        .then(response => Promise.all([
+            budget.createSector("budget-projet-craftcamp", "Infrastructure"),
+        ])),
+    budget.deleteBudget("budget-projet-medievistes")
+        .then(response => budget.createBudget("Budget Projet medievistes", "Budget du projet Medievistes"))
+        .then(response => Promise.all([
+            budget.createSector("budget-projet-medievistes", "Infrastructure"),
+        ])),
+]);
