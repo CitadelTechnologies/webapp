4d3765561cd70b74be54d6e5e0f499bd1ef2cc02
Axel Venet <kern046@gmail.com>
2018-05-30 23:01:29 +0200

    Fix project slug migration

--- migrations/20180410135813_project_slug.js
+++ migrations/20180410135813_project_slug.js
@@ -1,15 +1,20 @@
 const slug = require('slug');
-const model = require('../src/api/models');
+const model = require('../src/api/model');
 
-exports.up = (knex, Promise) => knex.schema.table('project__projects', table => {
-    table.string('slug');
-}).then(() => model.projects.findAll({}).then(projects => projects.map(project => {
-    project.slug = slug(project.name, { lower: true });
-    return model.projects.updateProject(project.id, project).then(result => {
-        console.log(result);
-        return result;
+exports.up = (knex, Promise) => knex.schema.hasColumn('project__projects', 'slug').then(hasColumn => {
+    if (hasColumn === true) {
+        return;
+    }
+    return knex.schema.table('project__projects', table => {
+        console.log('ok');
+        table.string('slug');
+        console.log('ouiii');
     });
-})));
+}).then(() => model.project.findAll({}))
+  .then(projects => projects.map(project => {
+    project.slug = slug(project.name, { lower: true });
+    return model.project.updateProject(project.id, project);
+}));
 
 exports.down = (knex, Promise) => knex.schema.table('project__projects', table => {
     table.dropColumn('slug');
